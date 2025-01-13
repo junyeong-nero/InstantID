75c1a6d8a481b099cf8255425b396c74be80fc4b
junyeong-nero <magicwho@unist.ac.kr>
2025-01-13 12:56:57 +0900

    update init.sh and requirements

--- init.sh
+++ init.sh
@@ -1,9 +1,11 @@
-cd /workspace/
-
 apt-get upgrade
 apt-get update
 apt-get install unzip
 
+echo "export HF_HOME=/workspace/cache" >> ~/.bashrc
+source ~/.bashrc
+
 python -m pip install --upgrade pip
-pip install -r gradio_demo/requirements.txt
+pip install -r gradio_demo/requirements.txt 
 python gradio_demo/download_models.py
+# python infer_full.py
